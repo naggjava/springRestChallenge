1f9ed122070dce4f8593b13fad4d1ada48f1a465
Nagamalleswara Gade <naggjava@gmail.com>
2020-08-14 20:02:59 -0400

    Converting JSON to Java List using Object Mapper.

--- src/main/java/com/streamlinity/ct/restService/challenge/SearchSvcImpl.java
+++ src/main/java/com/streamlinity/ct/restService/challenge/SearchSvcImpl.java
@@ -1,9 +1,19 @@
 package com.streamlinity.ct.restService.challenge;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.streamlinity.ct.model.Item;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.annotation.PostConstruct;
 
 /*
  * Provide your implementation of the SearchSvcImpl here.
@@ -15,15 +25,35 @@ import java.util.List;
  * PriceAdjustConfiguration
  */
 
+
 public class SearchSvcImpl implements SearchSvcInterface {
+	
+	
+	private List<Item> itemsInMemory;
+	
+	private ObjectMapper mapper;
+	
+	@PostConstruct
+	public void init() {
+		this.mapper = new ObjectMapper();
+	}
     @Override
     public void init(String itemPriceJsonFileName) {
-
+      try {
+		this.itemsInMemory =  mapper.readValue(itemPriceJsonFileName, new TypeReference() {});
+	  } catch (IOException e) {
+		e.printStackTrace();
+	  }
     }
 
     @Override
     public void init(File itemPriceJsonFile) {
 
+    	try {
+    		this.itemsInMemory =  mapper.readValue(itemPriceJsonFile, new TypeReference() {});
+    	  } catch (IOException e) {
+    		e.printStackTrace();
+    	  }
     }
 
     @Override
